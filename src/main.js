1062b0e4a1e0ed5b821c7c18e14e1243a0af23e7
Aryan Rajpoot <araryan1212@gmail.com>
2024-01-19 22:45:06 +0530

    added path to main.js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ const hbs = require("hbs");
 
 const port = process.env.PORT || 3000; 
 
+const static_path = path.join(__dirname, "../public" );  
+const template_path = path.join(__dirname, "../templates/views" );
+const partials_path = path.join(__dirname, "../templates/partials" );
+
 app.listen(port, () => {
     console.log(`server is running at port number ${port}`);
 })
